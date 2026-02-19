bb463fe3f3808e0856359cb20050a6bdda5e5c28
mrmario6969 <marioninjaaa@gmail.com>
2026-02-19 15:41:29 -0300

    Otimização da função showhide e textos
    mudei a função showhide para mudar o toggle da classe ao inves de mexer diretamente no css
    Também mudei os textos(toquei a cor e deixei o texto das infos maiores)
    
    nota: tenho q começar a dá commits individuais ao inves de tudo junto pfv eu do futuro dá commits individuais

--- onclick.js
+++ onclick.js
@@ -2,13 +2,8 @@
 
 function showhide(){
 console.log("entrou no clique");
-	const sh = document.getElementById("b_show");
-	if ( sh.style.display === "block"){
-		sh.style.display = "none";
-		console.log("none")
-	} else {
-		sh.style.display = "block";
-		console.log("block")
-	}
+
+    const sh = document.getElementById("b_show");
+    sh.classList.toggle("ativo");
 
  }
