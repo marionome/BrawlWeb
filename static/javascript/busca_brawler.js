b9088bafd475de9b2967fc09870dcb4274b370ef
mrmario6969 <marioninjaaa@gmail.com>
2026-07-19 17:08:22 -0300

    dados no html pronto
    agora a função de busca esta funcionando do jeito q deveria, agr só precisa mudar a estrutura do html pra acomodar a função

--- static/javascript/busca_brawler.js
+++ static/javascript/busca_brawler.js
@@ -1,7 +1,15 @@
-function buscar(){
+function buscar(nome){
 
-    fetch("http://127.0.0.1:5000/brawler/Poco", {method:"GET"})
-    .then(teste => teste.json())
-    .then(teste => console.log(teste))
+    fetch("http://127.0.0.1:5000/brawler/${nome}", {method:"GET"})
+    .then(brawldata => brawldata.json())
+    .then(brawldata => {
+        console.log("entrou")
+        document.getElementById("brawl_nome").innerHTML = brawldata.nome_brawler;
+        document.getElementById("classe").innerHTML = brawldata.classe;
+        document.getElementById("vida").innerHTML = brawldata.vida;
+        document.getElementById("dano").innerHTML = brawldata.dano;
+        document.getElementById("descricao").innerHTML = brawldata.descricao;
+        }
+    )
     
 }
